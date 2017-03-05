f07a476b8f22d7feacd13ffc2195621698acd770
Byung Woong Kwon <likebebop0@gmail.com>
2017-03-06 00:48:36 +0900

    stream test

--- src/test/java/com/jakewharton/u2020/data/Tutorial.java
+++ src/test/java/com/jakewharton/u2020/data/Tutorial.java
@@ -6,10 +6,16 @@ import com.annimon.stream.function.Function;
 
 import org.junit.Test;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.function.BiPredicate;
+
+import rx.functions.Func1;
+import rx.functions.Func2;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -69,6 +75,12 @@ public class Tutorial {
         System.out.printf("startWith %s => %s\n", startWith, r.orElse("not found"));
     }
 
+
+    static <T, U extends Comparable<? super U>> Comparator<T> comparing(java.util.function.Function<? super T, ? extends U> keyExtractor) {
+        return (c1, c2) -> keyExtractor.apply(c1).compareTo(keyExtractor.apply(c2));
+    }
+
+
     @Test
     public void testPractice() {
         transactions.stream().filter(t -> t.getYear() == 2011).sorted((o1, o2) -> o1.getValue() - o2.getValue()).forEach(System.out::println);
@@ -77,7 +89,7 @@ public class Tutorial {
 
         transactions.stream().filter(t -> t.getTrader().getCity().equals("Cambridge")).distinct().sorted((o1, o2) -> o1.getTrader().getName().compareTo(o2.getTrader().getName())).forEach(System.out::println);
 
-        //transactions.stream().filter(t->t.getTrader().getCity().equals("Cambridge")).map(t->t.getTrader()).distinct().sorted(Comparator.comparing(Trader::getName)).forEach(System.out::println);
+        transactions.stream().filter(t->t.getTrader().getCity().equals("Cambridge")).map(t->t.getTrader()).distinct().sorted(comparing(Trader::getName)).forEach(System.out::println);
 
         System.out.println(transactions.stream().map(t -> t.getTrader().getName()).distinct().sorted().reduce((a, b) -> {
             System.out.println(a + " " + b);
@@ -101,6 +113,12 @@ public class Tutorial {
     }
 
     @Test
+    public void testPractice2() {
+        transactions.stream().filter(t -> t.getTrader().getCity().equals("Cambridge")).sorted((o1, o2) -> o1.getTrader().getName().compareTo(o2.getTrader().getName())).forEach(System.out::println);
+        transactions.stream().filter(t->t.getTrader().getCity().equals("Cambridge")).map(Transaction::getTrader).distinct().sorted(comparing(Trader::getName)).forEach(System.out::println);
+    }
+
+    @Test
     public void testLamda() {
         List<Integer> list = new ArrayList();
         list.add(1);
@@ -110,6 +128,18 @@ public class Tutorial {
         Function<Boolean, Integer> f = a -> a ? 3 : 1;
         assertEquals(3, (int) f.apply(true));
         assertEquals(1, (int) f.apply(false));
+
+        Function<String, Integer> t = s->s.length();
+        Function<String, Integer> t_1 = String::length;
+
+        Func2<List<String>, String, Boolean> t2 = (l, s)->l.contains(s);
+        Func2<List<Integer>, Integer, Boolean> t2_1 = List::contains;
+
+        assertEquals(true, (boolean)t2_1.call(Arrays.asList(1, 2), 1));
+
+
+
+
     }
 
     static public class Dish {
