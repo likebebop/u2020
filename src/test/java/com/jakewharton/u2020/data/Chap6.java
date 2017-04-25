061926753dafc45fb4a3440917ac1e811e2fe2ff
bwkwon <bwkwon@linecorp.com>
2017-04-25 21:58:15 +0900

    java8 test

--- src/test/java/com/jakewharton/u2020/data/Chap6.java
+++ src/test/java/com/jakewharton/u2020/data/Chap6.java
@@ -2,6 +2,7 @@ package com.jakewharton.u2020.data;
 
 import com.annimon.stream.Collectors;
 import com.annimon.stream.Stream;
+import com.annimon.stream.function.BiFunction;
 
 import org.junit.Test;
 
@@ -20,10 +21,11 @@ public class Chap6 {
         System.out.println(Stream.of(Dish.menu).collect(Collectors.toList()));
         Map<Dish.Type, List<Dish>> map = Stream.of(Dish.menu).collect(Collectors.groupingBy(Dish::getType));
         System.out.println(map.toString());
-
         Stream.of(Dish.menu).groupBy(Dish::getType).forEach(System.out::println);
+        System.out.println("isVegetarian -> " + Stream.of(Dish.menu).filter(Dish::isVegetarian).peek(System.out::println).count());
 
-        //Stream<Map.Entry<Dish.Type, List<Dish>>> a = Stream.of(Dish.menu).collect();
+        System.out.println("All Calories 1 " + Stream.of(Dish.menu).reduce(0, (s, d) -> d.getCalories() + s));
+        System.out.println("All Calories 2 " + Stream.of(Dish.menu).map(Dish::getCalories).reduce(Integer::sum).get());
     }
 
 }
